966843563afed4a0aa5b4143251d5e522bbbecf8
mtnn <mtk8engr@gmail.com>
2026-04-10 13:31:14 +0900

    feat: osc7

--- wezterm.lua
+++ wezterm.lua
@@ -97,7 +97,7 @@ end)
 config.disable_default_key_bindings = true
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
-config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "e", mods = "SUPER", timeout_milliseconds = 2000 }
 
 ----------------------------------------------------
 -- Window Position
